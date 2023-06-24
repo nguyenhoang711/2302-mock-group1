31c936bdeb2346b285d15c8ea2dc12ef208a7cfd
Nguyen Hoang <metmoilamroiday4@gmail.com>
2023-06-24 17:04:42 +0700

    update
    update the UI of main page

--- Frontend/GroupManagementFrontEnd/src/routes/Routes.js
+++ Frontend/GroupManagementFrontEnd/src/routes/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
-  landing as landingRoutes,
+  // landing as landingRoutes,
   dashboard as dashboardRoutes,
   page as pageRoutes
 } from "./index";
