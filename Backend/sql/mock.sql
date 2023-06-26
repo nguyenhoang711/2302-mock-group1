eab9479e19f16c943feea6a03cd1cfdb1d113f5f
D521 <minhduchauik15@gmail.com>
2023-06-27 00:03:38 +0700

    update

--- Backend/sql/mock.sql
+++ Backend/sql/mock.sql
@@ -2,7 +2,7 @@
 USE testingsystem;
 
 -- them thong tin bang Tour
-INSERT INTO `tour`(details,duration,`name`,numOfPeople,thumbnailUrl,img1_url,img2_url,img3_url,img4_url,price,startDest,saleRate,`type`) VALUES
+INSERT INTO `tour`(details,duration,`name`,numOfPeople,thumbnail_url,img1_url,img2_url,img3_url,img4_url,price,startDest,saleRate,`type`) VALUES
 ('Ngày 1 - Săn Mây Cầu Đất - Cánh Đồng Điện Gió - Vườn Dâu - Vườn Hoa Cẩm Tú Cầu - Thưởng thức Trà Long Đỉnh','1 ngày', 'City Đà Lạt - Săn mây đón bình minh - Thưởng thức Trà Long Đình', 12,'DaLat_tour.jpg','','','','',790000,'Hà Nội',40,'STANDARD'),
 ('Nhân dịp kỷ niệm 30 năm Quần thể Di tích Cố đô Huế được UNESCO công nhận là Di sản Văn hóa Thế giới, 
 20 năm Nhã nhạc được công nhận Di sản Văn hóa Phi vật thể đại diện của nhân loại, Festival Huế 2023 với Chủ đề:
