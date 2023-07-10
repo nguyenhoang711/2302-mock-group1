c55763a0124e5b9a0d0cad91ba3a5bc7a18a1f87
LuckyLuke-nva <vietanh020600@gmail.com>
2023-07-10 23:04:31 +0700

    Trips
    update trips

--- Frontend/GroupManagementFrontEnd/src/api/TripApi.js
+++ Frontend/GroupManagementFrontEnd/src/api/TripApi.js
@@ -2,9 +2,33 @@ import Api from './Api';
 
 const url = "/trips";
 
+
+const getAll = (page = 1, size = 10, sortField = 'id', sortType = 'desc', search = '') => {
+
+    const parameters = {
+        page,
+        size,
+        sort: `${sortField},${sortType}`,
+        search
+    }
+
+    // search
+    if (search) {
+        parameters.search = search;
+    }
+
+    return Api.get(`${url}`, { params: parameters });
+};
+
 const getById = (id) => {
     return Api.get(`${url}/${id}`);
 };
 
-const api = {  getById }
+const existsById = (id) => {
+    return Api.get(`${url}/id/${id}`);
+};
+
+
+
+const api = { getById, getAll, existsById }
 export default api;
